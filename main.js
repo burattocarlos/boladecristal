5cdb2475419148a60ee38fbc623f17ca534bf630
Carlos Buratto Junior <30654838+burattocarlos@users.noreply.github.com>
2022-07-12 14:48:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const elementoResposta = document.querySelector("#resposta")
+const inputPergunta = document.querySelector("#inputPergunta")
+const buttonPerguntar = document.querySelector('#buttonPerguntar')
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "Não conte com isso.",
+  "Minhas fontes dizem não.",
+  "Não é possível prever agora.",
+  "Sim.",
+  "Sinais apontam que sim.",
+]
+
+// clicar em fazer pergunta
+function fazerPergunta() {
+
+  if(inputPergunta.value == "") {
+    alert("Digite sua pergunta")
+    return
+  }
+
+  buttonPerguntar.setAttribute("disabled", true)
+
+  const pergunta = "<div>" + inputPergunta.value + "</div>"
+
+  // gerar numero aletorio
+  const  totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+  elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+
+  elementoResposta.style.opacity = 1;
+
+  // sumir a resposta depois de 3 segundos
+  setTimeout(function() {
+    elementoResposta.style.opacity = 0;
+    buttonPerguntar.removeAttribute("disabled")
+  }, 3000)
+}
